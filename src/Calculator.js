0e5bf08c78f683fee35e310155dc5ca22b384d32
Jose Luis Alvarez <jalvarezh@psl.com.co>
2019-04-11 07:01:28 -0500

    Uploading to GitHub

---
+++ src/Calculator.js
@@ -0,0 +1,10 @@
+var Calculator = {
+    sum: function(number1, number2) {
+
+    },
+    divison: function(number1, number2) {
+
+    }
+}
+
+module.exports = Calculator;
---
+++ test/Calculator.spec.js
@@ -0,0 +1,48 @@
+var Calculator = require('../src/Calculator')
+var expect = require('chai').expect;
+var sinon = require('sinon');
+
+describe('Calculator', () => {
+
+    describe('Sum operation', () => {
+        it('Sum two positive integers', () => {
+            var result = Calculator.sum(5, 5);
+            expect(result).to.equal(10);
+        });
+    
+        it('Sum two negative integers', () => {
+            var result = Calculator.sum(-5, -5);
+            expect(result).to.equal(-10);
+        });
+    
+        it('Sum one positive and one negative integers', () => {
+            var result = Calculator.sum(-5, 5);
+            expect(result).to.equal(0);
+        });
+    
+    /*     it('Passing a character as the first number', () => {
+            var result = Calculator.sum('a', 5);
+            expect(result).to.equal('Not numbers');
+        });
+    
+        it('Passing a character as the second number', () => {
+            var result = Calculator.sum(5, 'a');
+            expect(result).to.equal('Not numbers');
+        });
+    
+        it('Passing a character as both numbers', () => {
+            var result = Calculator.sum('a', 'a');
+            expect(result).to.equal('Not numbers');
+        });
+        
+        it('Passing null as the first number', () => {
+            var result = Calculator.sum('a', 'a');
+            expect(result).to.equal('Not numbers');
+        });
+    
+        it('Sum stub', () => {
+            Calculator.sum = sinon.stub().withArgs(5, 5).returns(10);
+            expect(Calculator.sum(5, 5)).to.equal(10);
+        });*/
+    });    
+});
